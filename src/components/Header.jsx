dd7132914ac6bab87af6129d6926ecc2a8c9ca9b
Aditi Rai <aditi45@gmail.com>
2024-10-08 20:30:26 -0400

    addressing issues in project.jsx and adding styles

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Header = () => {
   return (
-    <header class="header">
+    <header className="header">
         <h1 id="h1">Aditi Rai</h1>
         <h2 id="h2">Software Developer</h2>
     </header>
--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -1,14 +1,36 @@
 import React from 'react'
+const styles = {
+    backgroundColor: '#004f8c',
+    color: 'white',
+    padding: '20px',
+    textAlign: 'center',
+    fontFamily: 'Arial'
+}
 
 const Project = () => {
   return (
-    <div>
-        <form>
-            <label for="contact">Contact:</label>
+    <div style={styles.backgroundColor}>
+        <h3>Projects</h3>
+        <ul>
+            <li>
+                <h4>Project 1</h4>
+                <p>Description of Project 1</p>
+            </li>
+            <li>
+                <h4>Project 2</h4>
+                <p>Description of Project 2</p>
+            </li>
+            <li>
+                <h4>Project 3</h4>
+                <p>Description of Project 3</p>
+            </li>
+        </ul>
+        <form className="form">
+            <label name="contact">Contact:</label>
             <input type="text" id="name" name="name" required></input>
-            <label for="email">Email Address:</label>
+            <label name="email">Email Address:</label>
             <input type="email" id="email" name="email" required></input>
-            <label for="message">Message:</label>
+            <label name="message">Message:</label>
             <textarea id="message" name="message" required></textarea>
             <button type="submit">Submit</button>
         </form>
