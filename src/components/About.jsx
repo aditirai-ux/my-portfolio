f15a1bf20764ee53b00369c694546560a12864ad
Aditi Rai <aditi45@gmail.com>
2024-10-09 21:50:27 -0400

    about page rework

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,9 +1,5 @@
 import React from 'react';
 import '../App.css';
-import { FontAwesomeIcon } 
-    from '@fortawesome/react-fontawesome'; 
-import { faUser } 
-    from '@fortawesome/free-solid-svg-icons';
 
 const About = () => {
   return (
@@ -11,17 +7,7 @@ const About = () => {
             className="container text-center"> 
             <div className="row"> 
                 <div className="col-md-8 offset-md-2"> 
-                    <h2 className="section-title"> 
-                        <FontAwesomeIcon icon={faUser} 
-                            className="mr-2" /> 
-                        About Me 
-                    </h2> 
-                    <div className="profile-image-container"> 
-
-                        <img src="/user.png"
-                            alt="geek pic"
-                            className="img-fluid rounded-circle profile-image" /> 
-                    </div> 
+                    <h2 className="section-title">About Me</h2> 
                     <p className="section-description mt-4"> 
                         I am a budding software developer with over 6 months of 
                         hands-on experience in full-stack web development. My 
