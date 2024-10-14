146347b7bfa71a5f5c2fd015da4ba8bb2dfbe14e
Aditi Rai <aditi45@gmail.com>
2024-10-14 09:51:20 -0400

    added routes to main.js

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,44 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
 import ReactDOM from 'react-dom/client';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import App from './App.jsx'
 import './index.css'
+import About from './components/About.jsx';
+import Home from './components/Home.jsx';
+import Project from './components/Project.jsx';
+import Contact from './components/Contact.jsx';
+import Resume from './components/Resume.jsx';
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        index: true,
+        element: <Home />,
+      },
+      {
+        path: '/About',
+        element: <About />,
+      },
+      {
+        path: '/Projects',
+        element: <Project />,
+      },
+      {
+        path: '/Resume',
+        element: <Resume />,
+      },
+      {
+        path: '/Contact',
+        element: <Contact />,
+      },
+    ],
+  },
+]);
+
+const rootElement = document.getElementById('root');
+if (rootElement) {
+  ReactDOM.createRoot(rootElement).render(<RouterProvider router={router} />);
+}
